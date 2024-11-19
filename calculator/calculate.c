ffd77996740079e91164883dba64f6dc2986e00f
Nomesh vyas <nomesh9351962867@gmail.com>
2024-11-19 19:22:00 +0530

    handling break cases

--- calculator/calculate.c
+++ calculator/calculate.c
@@ -4,14 +4,13 @@
 #define MAX 200
 
 // utility functions
-void printArr(int val[], int top){
-    while(top > - 1)
-        printf(" %d ", val[top--]);
-}
-
 int precendence(char op){
     if(op == '+' || op == '-') return 0;
     if(op == '*' || op == '/') return 1;
+}
+
+int isOp(char op){
+    return op == '+' || op == '-' || op == '*' || op == '/';
 }
 
 int oprationVal(char op, int a, int b, int* errCode){
@@ -35,29 +34,39 @@ int oprationVal(char op, int a, int b, int* errCode){
 int calculate(char exp[], int* errCode){
     int values[MAX];
     char operations[MAX];
-    // here top1 is for val, and top2 is for op
+    // here top1 is for values, and top2 is for operations
     int top1 = -1, top2 = -1;
     
     int i = 0;
     while(exp[i] != '\0'){
-        if('0' <= exp[i] && exp[i] <= '9'){
+        if(exp[i] == ' ' || exp[i] == '\n'){  // for space
+            i++;
+        } else if('0' <= exp[i] && exp[i] <= '9'){  // for number
             int num = exp[i++] - '0';
             
             while('0' <= exp[i] && exp[i] <= '9')
                 num = num * 10 + (exp[i++] - '0');
 
             values[++top1] = num;
-        } else if(exp[i] == '+' || exp[i] == '-' || exp[i] == '*' || exp[i] == '/'){
+        } else if(isOp(exp[i])){    // for operator
+            if(exp[i + 1] == '\0' || exp[i + 1] == '\n' || isOp(exp[i + 1] || (isOp(exp[i - 1]) && exp[i - 1] != '-'))){   // if 2 operators come 1 by 1 then we will send errCode 2
+                *errCode = 2;
+                return 0;
+            }
+            if(i - 1 < 0 && exp[i] == '-' && exp[i + 1] >= '0' && exp[i + 1] <= '9'){
+                values[++top1] = -(exp[i + 1] - '0');
+                i += 2;
+                continue;
+            }
+
             while(top2 >= 0 && precendence(operations[top2]) >= precendence(exp[i])){
                 char op = operations[top2--];
-                int a = values[top1--];
-                int b = values[top1];
-
-                int num = oprationVal(op, a, b, errCode);
-                values[top1] = num;
+                int b = values[top1--];
+                int a = values[top1];
+                int val = oprationVal(op, a, b, errCode);
+                values[top1] = val;
             }
-            operations[++top2] = exp[i];
-            i++;
+            operations[++top2] = exp[i++];
         } else {
             *errCode = 2;
             return 0;
@@ -69,32 +78,29 @@ int calculate(char exp[], int* errCode){
         int b = values[top1--];
         int a = values[top1];
 
-        int num = oprationVal(op, b, a, errCode);
-        values[top1] = num;
+        int val = oprationVal(op, a, b, errCode);
+        values[top1] = val;
     }
 
     return values[top1];
 }
 
 int main(){
-    char exp[] = "15+5*2-14/2";
+    char exp[MAX];
     char * ptr = exp;
-    int errCode = 0;
+    int errCode = 0;    // here we will use errCode 1 for divided by 0, and errCode 2 for invalid expression
 
     // taking the exp as input
-    // fgets(exp, MAX, stdin);
+    printf("Your Expression Calculator is here now...\n");
+    printf("Enter a valid expression for compute : \n");
+    fgets(exp, MAX, stdin);
     
-    // removing whitespace
-    int k = 0;
-    while(*ptr != '\0'){
-        if(*ptr != ' ')
-            exp[k++] = *ptr;
-        ptr++;
+    if(exp[0] == '\0' || exp[0] == '\n'){   // in case of user didn't gave any input
+        printf("Error: Invalid expression.");
+        return 0;
     }
-    exp[k] = '\0';
-    printf("%s", exp);
+
     int ans = calculate(exp, &errCode);
-    printf("%d\n", errCode);
 
     if(errCode == 1)
         printf("Error: Division by zero.");
